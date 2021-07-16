99c28104213d41bd58d65bccb3f0134858db73b0
Nguyễn Duy Thịnh <temthinh30012001@gmail.com>
2021-07-16 16:14:02 +0700

    first commit

---
+++ DentalPaymentForm/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace DentalPaymentForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTenKH = new System.Windows.Forms.TextBox();
+            this.cbCaoVoi = new System.Windows.Forms.CheckBox();
+            this.cbTayTrang = new System.Windows.Forms.CheckBox();
+            this.cbTaoHinhRang = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbTotal = new System.Windows.Forms.TextBox();
+            this.btnTinhTien = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(70, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(410, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dental Payment Form";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(73, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên khách hàng: ";
+            // 
+            // txtTenKH
+            // 
+            this.txtTenKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenKH.ForeColor = System.Drawing.Color.Red;
+            this.txtTenKH.Location = new System.Drawing.Point(260, 107);
+            this.txtTenKH.Name = "txtTenKH";
+            this.txtTenKH.Size = new System.Drawing.Size(247, 30);
+            this.txtTenKH.TabIndex = 2;
+            // 
+            // cbCaoVoi
+            // 
+            this.cbCaoVoi.AutoSize = true;
+            this.cbCaoVoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbCaoVoi.Location = new System.Drawing.Point(78, 190);
+            this.cbCaoVoi.Name = "cbCaoVoi";
+            this.cbCaoVoi.Size = new System.Drawing.Size(101, 29);
+            this.cbCaoVoi.TabIndex = 3;
+            this.cbCaoVoi.Text = "Cạo vôi";
+            this.cbCaoVoi.UseVisualStyleBackColor = true;
+            // 
+            // cbTayTrang
+            // 
+            this.cbTayTrang.AutoSize = true;
+            this.cbTayTrang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbTayTrang.Location = new System.Drawing.Point(78, 241);
+            this.cbTayTrang.Name = "cbTayTrang";
+            this.cbTayTrang.Size = new System.Drawing.Size(117, 29);
+            this.cbTayTrang.TabIndex = 3;
+            this.cbTayTrang.Text = "Tẩy trắng";
+            this.cbTayTrang.UseVisualStyleBackColor = true;
+            // 
+            // cbTaoHinhRang
+            // 
+            this.cbTaoHinhRang.AutoSize = true;
+            this.cbTaoHinhRang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbTaoHinhRang.Location = new System.Drawing.Point(78, 291);
+            this.cbTaoHinhRang.Name = "cbTaoHinhRang";
+            this.cbTaoHinhRang.Size = new System.Drawing.Size(155, 29);
+            this.cbTaoHinhRang.TabIndex = 3;
+            this.cbTaoHinhRang.Text = "Tạo hình răng";
+            this.cbTaoHinhRang.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(73, 362);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Trám răng";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(199, 367);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(78, 22);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(307, 431);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 25);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Total";
+            // 
+            // tbTotal
+            // 
+            this.tbTotal.Enabled = false;
+            this.tbTotal.Location = new System.Drawing.Point(380, 434);
+            this.tbTotal.Name = "tbTotal";
+            this.tbTotal.Size = new System.Drawing.Size(127, 22);
+            this.tbTotal.TabIndex = 5;
+            // 
+            // btnTinhTien
+            // 
+            this.btnTinhTien.Location = new System.Drawing.Point(78, 507);
+            this.btnTinhTien.Name = "btnTinhTien";
+            this.btnTinhTien.Size = new System.Drawing.Size(91, 35);
+            this.btnTinhTien.TabIndex = 6;
+            this.btnTinhTien.Text = "Tính tiền";
+            this.btnTinhTien.UseVisualStyleBackColor = true;
+            this.btnTinhTien.Click += new System.EventHandler(this.btnTinhTien_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(432, 507);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 35);
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(413, 190);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 25);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "$100.000";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(397, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 25);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "$1.200.000";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(413, 291);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 25);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "$200.000";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(393, 367);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(114, 25);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "$80.000/cái";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 570);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnTinhTien);
+            this.Controls.Add(this.tbTotal);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.cbTaoHinhRang);
+            this.Controls.Add(this.cbTayTrang);
+            this.Controls.Add(this.cbCaoVoi);
+            this.Controls.Add(this.txtTenKH);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTenKH;
+        private System.Windows.Forms.CheckBox cbCaoVoi;
+        private System.Windows.Forms.CheckBox cbTayTrang;
+        private System.Windows.Forms.CheckBox cbTaoHinhRang;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbTotal;
+        private System.Windows.Forms.Button btnTinhTien;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ DentalPaymentForm/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DentalPaymentForm
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int TinhTien()
+        {
+            int Total = 0;
+            if (cbCaoVoi.Checked) Total += 100000;
+            if (cbTayTrang.Checked) Total += 1200000;
+            if (cbTaoHinhRang.Checked) Total += 20000;
+            Total += Convert.ToInt32(numericUpDown1.Value) * 80000;
+            return Total;
+        }
+
+        private void btnTinhTien_Click(object sender, EventArgs e)
+        {
+            if (txtTenKH.Text == "")
+            {
+                MessageBox.Show("Vui long nhap ten KH!!");
+                return;
+            }
+            tbTotal.Text = "$" + TinhTien().ToString();
+        }
+    }
+}
